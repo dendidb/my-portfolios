1f65d05e4025eece7e2f087533a074f9730df984
Dendi Budiansyah <budidendi1234@gmail.com>
2022-04-11 14:03:24 +0700

    updated

--- covid-alert/src/scripts/components/Reveal.js
+++ covid-alert/src/scripts/components/Reveal.js
@@ -23,32 +23,10 @@ const Reveal = (() => {
 
     // -- homepage section
     ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.solution__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.solution__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.solution__img', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.solution__item', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.excellence__ttl', handleRevealConfig(50, '24px', 50, 'top'));
-    ScrollReveal().reveal('.excellence__item', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.sale__headline', handleRevealConfig(50, '24px', 50, 'left'));
-    ScrollReveal().reveal('.sale__title', handleRevealConfig(50, '24px', 60, 'left'));
-    ScrollReveal().reveal('.sale__desc', handleRevealConfig(50, '24px', 70, 'left'));
-    ScrollReveal().reveal('.sale__countdown', handleRevealConfig(50, '24px', 60, 'right'));
-    ScrollReveal().reveal('.benefits__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.benefits__img', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.benefits__item', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.presenting__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.presenting__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.testimonials__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.testimonials__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.documentation__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.documentation__card', handleRevealConfig(50, '24px', 50, 'bottom'));
 
     // -- responsive config
     if ($(window).width() <= 767.98) {
       ScrollReveal().reveal('.sale__headline', handleRevealConfig(50, '24px', 50, 'bottom'));
-      ScrollReveal().reveal('.sale__title', handleRevealConfig(50, '24px', 60, 'bottom'));
-      ScrollReveal().reveal('.sale__desc', handleRevealConfig(50, '24px', 70, 'bottom'));
-      ScrollReveal().reveal('.sale__countdown', handleRevealConfig(50, '24px', 60, 'bottom'));
     }
   }
 
