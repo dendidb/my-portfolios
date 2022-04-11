f179efe6ae7596d4a21e0a486bb9a21d8e968ac3
Dendi Budiansyah <budidendi1234@gmail.com>
2022-04-11 14:56:57 +0700

    updated

--- covid-alert/src/scripts/components/Reveal.js
+++ covid-alert/src/scripts/components/Reveal.js
@@ -25,13 +25,19 @@ const Reveal = (() => {
     ScrollReveal().reveal('.section-description__img', handleRevealConfig(50, '24px', 50, 'bottom'));
     ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
     ScrollReveal().reveal('.section-description__txt', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.problem__body', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.contagion__head', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.contagion__body', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.symptomps__head', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.symptomps__body', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.live-report__map', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.live-report__box', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.todo__head', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.todo__img', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.todo__txt', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.help__head', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.help__form', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.help__form__btn', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.todo__txt', handleRevealConfig(50, '24px', 50, 'bottom'));
 
     // -- responsive config
     if ($(window).width() <= 767.98) {
