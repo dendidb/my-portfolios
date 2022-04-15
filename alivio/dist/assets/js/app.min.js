f88fdb1fc5cf4dc4e629d36fa8bd3edb39f294d6
Dendi Budiansyah <budidendi1234@gmail.com>
2022-04-15 10:03:56 +0700

    updated

--- alivio/dist/assets/js/app.min.js
+++ alivio/dist/assets/js/app.min.js
@@ -480,37 +480,17 @@ var Reveal = function () {
 
 
   var handleRunScrollReveal = function handleRunScrollReveal() {
-    ScrollReveal().reveal('.js-reveal .product-card', handleRevealConfig());
-    ScrollReveal().reveal('.title-page__txt', handleRevealConfig()); // -- section-description section
+    // -- presenting section
+    ScrollReveal().reveal('.presenting__left', handleRevealConfig(50, '24px', 50, 'left'));
+    ScrollReveal().reveal('.presenting__right', handleRevealConfig(50, '24px', 50, 'right')); // -- features-item section
 
-    ScrollReveal().reveal('.section-description__box', handleRevealConfig(50, '24px', 50, 'bottom'));
+    ScrollReveal().reveal('.features__item', handleRevealConfig(50, '24px', 50, 'bottom')); // -- success-story section
 
-    if ($(window).width() >= 992.98) {} // -- expertise section
+    ScrollReveal().reveal('.success-story__img', handleRevealConfig(50, '24px', 50, 'left'));
+    ScrollReveal().reveal('.success-story__txt', handleRevealConfig(50, '24px', 50, 'right')); // -- our-product section
 
-
-    ScrollReveal().reveal('.expertise__title', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.expertise__desc', handleRevealConfig(50, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.expertise__list', handleRevealConfig(60, '24px', 50, 'bottom')); // -- client section
-
-    ScrollReveal().reveal('.client__title', handleRevealConfig(60, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.client__desc', handleRevealConfig(60, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.client__list', handleRevealConfig(60, '24px', 50, 'bottom')); // -- portfolio section
-
-    ScrollReveal().reveal('.portofolio__item', handleRevealConfig(55, '24px', 50, 'bottom')); // -- contact-us section
-
-    ScrollReveal().reveal('.contact-us__title', handleRevealConfig(55, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.contact-us__desc', handleRevealConfig(55, '24px', 50, 'bottom'));
-    ScrollReveal().reveal('.contact-us__img', handleRevealConfig(55, '24px', 50, 'left'));
-    ScrollReveal().reveal('.contact-us__form', handleRevealConfig(55, '24px', 50, 'right')); // -- footer
-
-    ScrollReveal().reveal('.footer__copyright', handleRevealConfig(55, '24px', 50, 'bottom')); // -- responsive config
-
-    if ($(window).width() <= 767.98) {
-      ScrollReveal().reveal('.portofolio__list', handleRevealConfig(50, '24px', 50, 'bottom'));
-      ScrollReveal().reveal('.expertise__list', handleRevealConfig(50, '24px', 50, 'bottom'));
-      ScrollReveal().reveal('.contact-us__img', handleRevealConfig(55, '24px', 50, 'bottom'));
-      ScrollReveal().reveal('.contact-us__form', handleRevealConfig(55, '24px', 50, 'bottom'));
-    }
+    ScrollReveal().reveal('.our-product__img', handleRevealConfig(50, '24px', 50, 'right'));
+    ScrollReveal().reveal('.our-product__txt', handleRevealConfig(50, '24px', 50, 'left'));
   }; // --- init
 
 
